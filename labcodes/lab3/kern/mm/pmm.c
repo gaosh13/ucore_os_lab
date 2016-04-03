ed5ca206aa085563f77f50832385eb99c17bc3e9
littlefriend <gsh0818@126.com>
2016-04-03 20:37:50 +0800

    lab3

--- labcodes/lab3/kern/mm/pmm.c
+++ labcodes/lab3/kern/mm/pmm.c
@@ -360,7 +360,7 @@ pmm_init(void) {
 // return vaule: the kernel virtual address of this pte
 pte_t *
 get_pte(pde_t *pgdir, uintptr_t la, bool create) {
-    /* LAB2 EXERCISE 2: YOUR CODE
+    /* LAB2 EXERCISE 2: 2013011400
      *
      * If you need to visit a physical address, please use KADDR()
      * please read pmm.h for useful macros
@@ -381,6 +381,19 @@ get_pte(pde_t *pgdir, uintptr_t la, bool create) {
      *   PTE_W           0x002                   // page table/directory entry flags bit : Writeable
      *   PTE_U           0x004                   // page table/directory entry flags bit : User can access
      */
+    pde_t *pde = pgdir + PDX(la);
+    if (!(*pde & PTE_P))
+    {
+        if (!create) return NULL;
+        struct Page *page;
+        page = alloc_page();
+        if (!page) return NULL;
+        set_page_ref(page, 1);
+        uintptr_t pa = page2pa(page);
+        memset(KADDR(pa), 0, PGSIZE);
+        *pde = pa | PTE_U | PTE_W | PTE_P;
+    }
+    return ((pte_t*)KADDR(PDE_ADDR(*pde))) + PTX(la);
 #if 0
     pde_t *pdep = NULL;   // (1) find page directory entry
     if (0) {              // (2) check if entry is not present
@@ -413,7 +426,7 @@ get_page(pde_t *pgdir, uintptr_t la, pte_t **ptep_store) {
 //note: PT is changed, so the TLB need to be invalidate 
 static inline void
 page_remove_pte(pde_t *pgdir, uintptr_t la, pte_t *ptep) {
-    /* LAB2 EXERCISE 3: YOUR CODE
+    /* LAB2 EXERCISE 3: 2013011400
      *
      * Please check if ptep is valid, and tlb must be manually updated if mapping is updated
      *
@@ -429,6 +442,14 @@ page_remove_pte(pde_t *pgdir, uintptr_t la, pte_t *ptep) {
      * DEFINEs:
      *   PTE_P           0x001                   // page table/directory entry flags bit : Present
      */
+    if (*ptep & PTE_P)
+    {
+        struct Page *page = pte2page(*ptep);
+        if (page_ref_dec(page) == 0)
+            free_page(page);
+        *ptep = 0;
+        tlb_invalidate(pgdir, la);
+    }
 #if 0
     if (0) {                      //(1) check if this page table entry is present
         struct Page *page = NULL; //(2) find corresponding page to pte
